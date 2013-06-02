2b1512e9500737a01c0a2d5d9ff4ad5d4674995a
Rich Hart <RichHOHart@gmail.com>
2013-06-02 17:15:26 -0400

    more changes

--- assignment2.cpp
+++ assignment2.cpp
@@ -22,8 +22,7 @@ init( void )
 {
     // Specifiy the vertices for a triangle
     vec2 vertices[NumPoints];
- 	vec2 temp[NumPoints];
-    temp = {
+ 	vec2 temp[NumPoints] = {
 
        // vec2( 0.0, 0.0 ), vec2( -1.0, 0.0 ), vec2( 0.0, -1.0 ),
 
@@ -48,7 +47,7 @@ vec2(scale*(0.0+x_offest),scale*(0.0+y_offest)),vec2(scale*(sin(4*PI/5)+x_offest
 vec2(scale*(0.0+x_offest),scale*(0.0+y_offest)),vec2(scale*(0.0+x_offest),scale*(1.0+y_offest)),vec2(scale*(sin(2*PI/5)+x_offest),scale*(cos(2*PI/5)+y_offest))
     };
 
-	vertices=temp;
+	vertices*=temp*;
     // Create a vertex array object
     GLuint vao[1];
     glGenVertexArrays( 1, vao );
