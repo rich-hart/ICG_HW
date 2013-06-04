32b1fd9a51c26e005d632389e4f17a7d89a5825c
Rich Hart <RichHOHart@gmail.com>
2013-06-04 12:36:47 -0400

    shader file was saved this time

--- vshader21.glsl
+++ vshader21.glsl
@@ -12,28 +12,8 @@ void
 main()
 {
     gl_Position = vPosition;
-	float type = vColor.t;
-	float cx = vColor.x;
-	float cy = vColor.y;
-	float side = vColor.z;
-	
-	if(0==1){
-	
-	float hieght = sqrt(pow(side,2.0) - pow(side/2.0,2.0));
-	vec2 A = vec2( cx-side/2.0, cy -hieght/2.0);
-	vec2 B =vec2( cx+side-side/2.0, cy -hieght/2.0);
-	vec2 C   = vec2( cx+side/2.0-side/2.0, cy+ hieght-hieght/2.0);
-	
-	float da=Distance(A.x,A.y,vPosition.x,vPosition.y);
-	float db=Distance(B.x,B.y,vPosition.x,vPosition.y);
-	float dc=Distance(C.x,C.y,vPosition.x,vPosition.y);
-	
-	
-//	fColor=vec4(da/hieght,db/hieght,dc/hieght,1.0);
-	fColor=vec4(1.0,0.0,0.0,1.0);	
-	} else{
 	
 	
 	fColor = vColor;
-	}
+	
 }
